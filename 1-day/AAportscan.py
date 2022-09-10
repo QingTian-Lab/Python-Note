6da6b679e6b1c7c132b94c9a7c50bbea7a5025d4
woOzZ2 <woOzZ2>
2022-09-10 21:56:25 +0800

    设计到项目py文件调用语法不能以数字开头，题目前缀换为"AA"

--- 1-day/AAportscan.py
+++ 1-day/AAportscan.py
@@ -1,11 +1,11 @@
 import sys, socket, threading
 
 showerror = False  # 连接异常信息输出
-pool_sema = threading.BoundedSemaphore(1000) # 线程数限制  不限制会卡死
-
+pool_sema = threading.BoundedSemaphore(1) # 线程数限制  不限制会卡死
+print("fuck")
 
 def ScannerThread(TargetIp, Port=80):
-    pool_sema.acquire()  # 线程数 的锁 忽略
+    pool_sema.acquire()  # 设置线程锁
     try: # 异常处理头
         sock = socket.socket() # 初始化一个socket，并且传递给sock
         sock.settimeout(3) # 设置超时时间
@@ -16,9 +16,9 @@ def ScannerThread(TargetIp, Port=80):
         if showerror: # 如果该值为True 会执行print(eror)
             print(error)
         else: # 否则 不执行任何代码
-            pass
-    pool_sema.release() # 线程数 的锁 忽略
-
+            print("false")
+    pool_sema.release() # 释放线程锁
+print(__name__)
 
 if __name__ == "__main__":
     print("pscan version 1.0")
@@ -32,6 +32,7 @@ if __name__ == "__main__":
         # args 为传递给ScannerThread的参数 分别为：sys.argv 也就是第一个参数地址  第二个为i也就是端口
         ThreadObject.setDaemon(True) # 设置守护线程 如果主线程崩了 别的线程一起中断执行
         ThreadList.append(ThreadObject) # 将线程对象加入到列表里
+        ThreadObject.setName("Port:%d"%i)
     del ThreadObject  # 删除ThradObject 这样后面使用同样的命名变量函数不会起冲突
 
     for ThreadObject in ThreadList: # 循环线程对象列表
